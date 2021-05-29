1cf5b026a5aab5a12023b096a4dfb5b652387b35
jsshin <jsshin@salin.co.kr>
2021-05-29 14:50:20 +0900

    call receive candidate 기능구현

--- src/lib/websocket/channelHelper.ts
+++ src/lib/websocket/channelHelper.ts
@@ -1,37 +1,43 @@
 // 인스턴스까지 필요 없다
 import { Message } from './actions/receive'
 import Session from './Session'
-import { coreRedisClient } from './redis/createRedisClient'
+import { coreRedisClient, publishJSON } from './redis/createRedisClient'
 import { promisify } from 'util'
+import prefixer from './redis/prefixer'
+import actionCreators from './actions/send'
 
-const withPrefix = (channel: string) => `channel:${channel}`
-const createSessionsKey = (channel: string) => `${withPrefix(channel)}:users`
+// const withPrefix = (channel: string) => `channel:${channel}`
+// const createSessionsKey = (channel: string) => `${withPrefix(channel)}:users`
 
 const channelHelper = {
   enter(channel: string, sessionId: string) {
+    publishJSON(prefixer.channel(channel), actionCreators.entered(sessionId))
     coreRedisClient.publish(
-      withPrefix(channel),
+      prefixer.channel(channel),
       JSON.stringify({
         type: 'enter',
         sessionId: sessionId,
       })
     )
-    coreRedisClient.lpush(createSessionsKey(channel), sessionId)
+    coreRedisClient.lpush(prefixer.sessions(channel), sessionId)
   },
 
   leave(channel: string, sessionId: string) {
+    publishJSON(prefixer.channel(channel), actionCreators.left(sessionId))
     coreRedisClient.publish(
-      withPrefix(channel),
+      prefixer.channel(channel),
       JSON.stringify({
         type: 'leave',
         sessionId: sessionId,
       })
     )
-    coreRedisClient.lrem(createSessionsKey(channel), 1, sessionId)
+    coreRedisClient.lrem(prefixer.sessions(channel), 1, sessionId)
   },
   message(channel: string, sessionId: string, message: Message) {
+    publishJSON(prefixer.channel(channel), actionCreators.messaged(sessionId, message))
+
     coreRedisClient.publish(
-      withPrefix(channel),
+      prefixer.channel(channel),
       JSON.stringify({
         type: 'leave',
         sessionId,
@@ -40,7 +46,7 @@ const channelHelper = {
     )
   },
   async listSessions(channel: string) {
-    const key = createSessionsKey(channel)
+    const key = prefixer.sessions(channel)
     // this가 무엇인지를 몰라 bind를 해야한다
     const lrangeAsync = promisify(coreRedisClient.lrange).bind(coreRedisClient)
     return await lrangeAsync(key, 0, -1)
