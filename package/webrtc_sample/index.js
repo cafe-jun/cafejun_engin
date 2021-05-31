6ef754d9677bc8e0cf9a42135ed46048c28dd073
jsshin <jsshin@salin.co.kr>
2021-05-31 22:54:39 +0900

    WebRTC Video Streams Setting

--- package/webrtc_sample/index.js
+++ package/webrtc_sample/index.js
@@ -1,26 +1,63 @@
 import './style.css';
 
-const ws = new WebSocket('ws://127.0.0.1:8081/websocket');
+const ws = new WebSocket('ws://localhost:8081/websocket');
+const myVideo = document.body.querySelector('#myVideo');
+
+const rtcConfiguration = {};
+
 ws.addEventListener('message', (event) => {
     handleMessage(event.data.toString());
 });
-
-const rtcConfigureation = {};
-
-let localStream = null;
-let localPeer = null;
 
 function sendJSON(object) {
     const message = JSON.stringify(object);
     ws.send(message);
 }
 
+let sessionId = null;
+let localPeer = null;
+
+function handleMessage(message) {
+    try {
+        const action = JSON.parse(message);
+        if (!action.type) {
+            throw new Error('There is no type in action');
+        }
+
+        switch (action.type) {
+            case 'connected':
+                console.log(`sessionId: ${action.id}`);
+                sessionId = action.id;
+                break;
+            case 'entered':
+                if (action.sessionId === sessionId) {
+                    break;
+                }
+                call(action.sessionId);
+                break;
+            case 'called':
+                answer(action.from, action.description);
+                break;
+            case 'answered':
+                answered(action.from, action.description);
+                break;
+            case 'candidated':
+                candidated(action.from, action.candidate);
+                break;
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const channelForm = document.querySelector('#channelForm');
+
+let localStream = null;
+
 async function initializeStream() {
     try {
-        localStream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: true,
-        });
+        localStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+        console.log('Received localStream');
     } catch (e) {
         console.error(e);
     }
@@ -36,46 +73,51 @@ function enterChannel(channelName) {
 async function call(to) {
     if (!localStream) return;
 
-    localPeer = new RTCPeerConnection(rtcConfigureation);
+    localPeer = new RTCPeerConnection(rtcConfiguration);
     localPeer.addEventListener('icecandidate', (e) => {
-        console.log('icecandidate');
         icecandidate(to, e.candidate);
+    });
+    localPeer.addEventListener('track', (ev) => {
+        if (myVideo.srcObject !== ev.streams[0]) {
+            myVideo.srcObject = ev.streams[0];
+        }
     });
 
     localStream.getTracks().forEach((track) => {
-        localPeer.addTrack(track);
+        localPeer.addTrack(track, localStream);
     });
 
     const offer = await localPeer.createOffer();
-    console.log(`offer: ${offer}`);
+    console.log('offer: ', offer);
     await localPeer.setLocalDescription(offer);
 
-    //const remotePeer =
     sendJSON({
         type: 'call',
         to,
         description: offer,
     });
-    console.dir(`LocalStream : ${localStream}`);
 }
 
 async function answer(to, description) {
-    console.log(`LocalStream : ${localStream}`);
-
     if (!localStream) return;
 
-    localPeer = new RTCPeerConnection(rtcConfigureation);
+    localPeer = new RTCPeerConnection(rtcConfiguration);
     localPeer.addEventListener('icecandidate', (e) => {
         icecandidate(to, e.candidate);
-        console.log('icecandidate');
     });
+    localPeer.addEventListener('track', (ev) => {
+        if (myVideo.srcObject !== ev.streams[0]) {
+            myVideo.srcObject = ev.streams[0];
+        }
+    });
+
     localStream.getTracks().forEach((track) => {
         localPeer.addTrack(track, localStream);
     });
-    console.log('여기 실행?');
+
     await localPeer.setRemoteDescription(description);
     const answer = await localPeer.createAnswer();
-    localPeer.setLocalDescription(answer);
+    await localPeer.setLocalDescription(answer);
 
     sendJSON({
         type: 'answer',
@@ -84,21 +126,12 @@ async function answer(to, description) {
     });
 }
 
-async function candidated(from, candidate) {
-    try {
-        localPeer.addIceCandidate(candidate);
-        console.log(`Candidated ${from} Success`);
-    } catch (e) {
-        console.error(`Fail to candidate ${e.toString()}`);
-    }
-}
-
 async function answered(from, description) {
     await localPeer.setRemoteDescription(description);
-    console.log(`Answered Call from Success for ${from}`);
+    console.log(`setRemoteDescription success for ${from}`);
 }
 
-async function icecandidate(to, candidate) {
+function icecandidate(to, candidate) {
     sendJSON({
         type: 'candidate',
         to,
@@ -106,82 +139,19 @@ async function icecandidate(to, candidate) {
     });
 }
 
-let sessionId = null;
-
-function handleMessage(message) {
+function candidated(from, candidate) {
     try {
-        //console.log(`message : ${typeof message}`);
-        const action = JSON.parse(message);
-        //console.dir(`action : ${typeof action}`);
-        //console.log(`action type : ${action.type}`);
-        if (!action.type) {
-            throw new Error('There is no Type');
-        }
-        console.log(action);
-        switch (action.type) {
-            case 'connected':
-                console.log(`sessionId : ${action.id}`);
-                sessionId = action.id;
-                break;
-            case 'entered':
-                console.dir(`${action.sessionId}`);
-                if (action.sessionId === sessionId) {
-                    console.log('entered Success');
-                    break;
-                }
-                console.log(`Call ${action.sessionId}`);
-                call(action.sessionId);
-                break;
-            case 'called':
-                console.log(`called ${action.from}`);
-                answer(action.from, action.description);
-                break;
-            case 'answered':
-                console.log(`Answered : ${action.from}`);
-                answered(action.from, action.description);
-                break;
-            case 'candidated':
-                candidated(action.from, action.candidate);
-                break;
-        }
+        localPeer.addIceCandidate(candidate);
+        console.log(`Candidate from ${from} success!`);
     } catch (e) {
-        //console.error(e);
-        console.error(`Fail to parse message : ${message}`);
+        console.error(`Failed to candidate: ${e.toString()}`);
     }
 }
 
-const channelForm = document.querySelector('#channelForm');
-
 channelForm.addEventListener('submit', async (e) => {
-    //console.log(e.target.channelName.value);
     channelForm.querySelector('button').disabled = true;
-
-    // sendJSON(
-    //     JSON.stringify({
-    //         type: 'enter',
-    //         channel: e.target.channelName.value,
-    //     }),
-    // );
     e.preventDefault();
+
     await initializeStream();
     enterChannel(channelForm.channelName.value);
 });
-//const button = document.body.querySelector('#binLoadCam');
-// const myVideo = document.body.querySelector('#myVideo');
-
-// async function loadCamera() {
-//     try {
-//         const stream = await navigator.mediaDevices.getUserMedia({
-//             audio: true,
-//             video: true,
-//         });
-//         const videoTrack = stream.getVideoTracks();
-//         myVideo.srcObject = stream;
-//     } catch (e) {
-//         console.error(e);
-//     }
-// }
-
-// button.addEventListener('click', () => {
-//     loadCamera();
-// });
